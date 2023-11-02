da56cd2cd79a80232489c6363605e83b26a5c1ac
c2hankhue <135484707+c2hankhue@users.noreply.github.com>
2023-11-02 21:10:52 +0700

    Create info.php

---
+++ info.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company Introduction</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Company</h1>
+    </header>
+
+    <section>
+        <h2>About Us</h2>
+        <p>We are a dynamic company headquartered in Vietnam, dedicated to providing top-notch labor services globally.</p>
+    </section>
+
+    <section>
+        <h2>Our Team</h2>
+        <p>With a team of 100 skilled and motivated employees, we strive for excellence in every aspect of our operations.</p>
+    </section>
+
+    <section>
+        <h2>Global Reach</h2>
+        <p>We specialize in exporting labor to countries such as Japan, Korea, and beyond. Our workforce is known for its reliability and professionalism.</p>
+    </section>
+
+    <footer>
+        <p>Contact us for more information: info@yourcompany.com</p>
+    </footer>
+</body>
+</html>
